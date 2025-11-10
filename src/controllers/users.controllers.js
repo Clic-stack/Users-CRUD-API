a2d4adae105a65e7c2d58bf821261984e4b7a0dc
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2025-11-10 13:24:31 -0600

    Third commit: Updated message CRUD in users.controllers.js

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -16,22 +16,16 @@ export const createUser = async (req, res) => {
   try {
     const { first_name, last_name, email, password, birthday, image_url } = req.body
 
-    // Si no se envía image_url, genera uno con UI Avatars
+    // Si no se envía image_url, genera uno con UI Avatars / A UI Avatars image is generated if image_url is ommited
     const avatar = image_url || `https://ui-avatars.com/api/?name=${first_name}+${last_name}`
 
     const user = await User.create({ first_name, last_name, email, password, birthday, image_url: avatar })
     res.status(201).json({ message: 'User created', user })
   } catch (error) {
-    console.error('Error al crear usuario:', error)
-    res.status(500).json({ message: 'Error al crear usuario', error: error.message })
+    console.error('Error creating user:', error)
+    res.status(500).json({ message: 'Error creating user', error: error.message })
   }
 }
-
-/*export const createUser = async (req, res) => {
-    const { first_name, last_name, email, password, birthday, image_url } = req.body
-    const user = await User.create({ first_name, last_name, email, password, birthday, image_url })
-    res. send({message: 'User created', user: user})
-}*/
 
 export const updateUser = async (req, res) => {
   try {
@@ -41,7 +35,7 @@ export const updateUser = async (req, res) => {
     const user = await User.findByPk(id)
     if (!user) return res.status(404).json({ message: 'User not found' })
 
-    // Si no se envía image_url, genera uno nuevo con UI Avatars
+    // Si no se envía image_url, genera uno nuevo con UI Avatars / A UI Avatars image is generated if image_url is ommited
     const avatar = image_url || `https://ui-avatars.com/api/?name=${first_name}+${last_name}`
 
     const updatedUser = await user.update({
@@ -55,17 +49,10 @@ export const updateUser = async (req, res) => {
 
     res.json({ message: 'User updated', user: updatedUser })
   } catch (error) {
-    console.error('Error al actualizar usuario:', error)
-    res.status(500).json({ message: 'Error al actualizar usuario', error: error.message })
+    console.error('Error updating user:', error)
+    res.status(500).json({ message: 'Error updating user', error: error.message })
   }
 }
-
-/*export const updateUser = async (req, res) => {
-    const { id } = req.params
-    const { first_name, last_name, email, password, birthday, image_url } = req.body
-    const updateUser = await User.update({ first_name, last_name, email, password, birthday, image_url }, { where: {id}, returning: true })
-    res.send({ message: 'User updated', user: updateUser[1][0] })
-}*/
 
 export const deleteUser = async (req, res) => {
     const { id } = req.params
