206b6cae6dd28930c71d4b6400275ff59b53a695
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2025-11-11 20:05:42 -0600

    Updated CORSurl

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://usercrud-fullstack.netlify.app"
+    "https://usercrud-fullstack.netlify.app/"
   ]
 }));
 
