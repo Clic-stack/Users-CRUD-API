187b16c19a61744ced4002cb997b7710e7feecab
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2025-11-11 20:13:33 -0600

    Changes CORS configuration

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ const app = express()
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://usercrud-fullstack.netlify.app/"
-  ]
+    "https://usercrud-fullstack.netlify.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+  allowedHeaders: ["Content-Type"]          
 }));
 
 app.use(express.json())
